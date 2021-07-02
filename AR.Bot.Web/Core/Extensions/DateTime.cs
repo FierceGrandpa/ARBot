c45d61ca639a6ff36e46f0ebe5b70fcc0b131ecd
timickb <timikm2002@gmail.com>
2021-07-03 00:08:57 +0500

    Added DateTime extension

---
+++ AR.Bot.Web/Core/Extensions/DateTime.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AR.Bot
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime ToMoscowTime(this DateTime dt)
+        {
+            return dt.ToUniversalTime().AddHours(3);
+        }
+        
+    }
+}
